e54744bcb269481876466f7269162954488ed7bc
Tsundoiii <o2oxo0dbv@relay.firefox.com>
2022-04-25 18:25:35 -0400

    tile test

--- script.js
+++ script.js
@@ -8,16 +8,28 @@ class Tile {
     constructor(row, column) {
         this.x = row;
         this.column = column;
-        this.id = `${row}${column}`;
+        this.id = `r${row}c${column}`;
     }
 }
 
-function createBoard(width, length) {
+/**
+ * Create a tetris board of specified width and height
+ * 
+ * @param {number} width The width of the board
+ * @param {number} height The height of the board
+ * @param {object} board The element where the board will go
+ */
+function createBoard(width, height, board) {
     for (let row = 0; row < width; row++) {
-        for (let column = 0; column < width; column++) {
-            
+        for (let column = 0; column < height; column++) {
+            const tile = new Tile(row, column);
+            board.innerHTML += `<div id="${tile.id}" class="tile r${tile.row} c${tile.column}"`;
         }
+
+        board.innerHTML += "<br>"
     }
 }
 
 // Main code
+
+createBoard(2, 2, document.getElementById("board"));
