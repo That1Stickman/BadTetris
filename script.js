c3bb8ba66b6194fa2153f86f3faeaba4d465b197
unknown <o2oxo0dbv@relay.firefox.com>
2022-04-25 10:44:07 -0400

    added comment

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
  * 
  * @param {number} x The x value (width) of the board
  * @param {number} y The y value (length) of the board
+ * @param {object} board The div which the board is to be created
  */
  function createBoard(x, y, board) {
     for (let i = x; i < y; i++) {
