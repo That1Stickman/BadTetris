56ee27f1c9030aede50c4bf6bc40e217d166379e
unknown <o2oxo0dbv@relay.firefox.com>
2022-04-25 10:19:07 -0400

    test pls ignore

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * Create the tetris board
+ * 
+ * @param {number} x The x value (width) of the board
+ * @param {number} y The y value (length) of the board
+ */
+ function createBoard(x, y, board) {
+    for (let i = x; i < y; i++) {
+
+    }
+}
+
+// Main code
+
+const b = document.getElementById("board")
+console.log(typeof b)
