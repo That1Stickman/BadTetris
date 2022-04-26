63c9f75c8ddec93a535e403e8562ce98da982355
Tsundoiii <o2oxo0dbv@relay.firefox.com>
2022-04-26 14:35:30 -0400

    made piece interface

--- script.js
+++ script.js
@@ -19,4 +19,11 @@ function createBoard(width, height) {
     }
     return board;
 }
+const I = { color: "cyan" };
+const J = { color: "blue" };
+const L = { color: "orange" };
+const O = { color: "yellow" };
+const S = { color: "green" };
+const T = { color: "purple" };
+const Z = { color: "red" };
 document.getElementById("board").innerHTML = createBoard(4, 3);
