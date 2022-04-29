25e88a9606ded7ba30f779761d3e19ec85af1dbe
Samuel Vrtismarsh <104369531+That1Stickman@users.noreply.github.com>
2022-04-29 14:35:16 +0000

    Formed Pieces

--- script.js
+++ script.js
@@ -1,44 +1,93 @@
 class Tile {
-    constructor(row, column) {
+    constructor(row, column, state) {
         this.row = row;
         this.column = column;
         this.id = `r${row}c${column}`;
-        this.state = "unfilled";
+        this.state = state;
         this.color = null;
     }
 }
 function createBoard(width, height, board) {
-  if (width>=4){
-    if (height>=3){
+  if (width>=3){
+    if (height>=4){
       document.getElementById("board").innerHTML="";
-    for (var i = 1; i <= height; i++) {
+    for (var i = 1; i <= width; i++) {
         var row = "";
-        for (var j = 1; j <= width; j++) {
-            var tile = new Tile(i, j);
-            row += "<div id=\"".concat(tile.id, "\" class=\"tile r").concat(tile.row, " c").concat(tile.column, "\"></div>");
+        for (var j = 1; j <= height; j++) {
+            var tile = new Tile(i, j,0);
+            row += "<div id=\"".concat(tile.id), "\" class='tile'></div>";
         }
-        board.innerHTML += "<div id=\"r".concat(i, "\" class=\"row\">").concat(row, "</div>");
+        board.innerHTML += "<div id=\"r".concat(i, "\" class='tile'></div>");
     }
-  document.getElementById("board").style.minWidth= (width*30)+"px";
+  document.getElementById("board").style.minWidth= (height*30)+"px";
   document.getElementById("definerows").value = "";
   document.getElementById("definecolumns").value = "";
                 }
     }
-  
+  BoardSpawn = CreateSpawn(height);
   
 }
 
 class Piece{
   //setInterval(pieceUpdate, gamespeed)
+  
 }
 const I = { 
-  color: "cyan" 
-  
+  color: "cyan",
+  pieceShape: [
+    [1,1,1,1],
+    [0,0,0,0]
+  ]
+}
+const J = {
+  color: "blue",
+  pieceShape: [
+    [1,0,0,0],
+    [1,1,1,0]
+  ]
 };
-const J = { color: "blue" };
-const L = { color: "orange" };
-const O = { color: "yellow" };
-const S = { color: "green" };
-const T = { color: "purple" };
-const Z = { color: "red" };
+const L = {
+  color: "orange",
+  pieceShape: [
+    [0,0,0,1],
+    [0,1,1,1]
+  ]
+};
+const O = {
+  color: "yellow",
+  pieceShape: [
+    [0,1,1,0],
+    [0,1,1,0]
+  ]
+};
+const S = {
+  color: "green",
+  pieceShape: [
+    [0,1,1,0],
+    [1,1,0,0]
+  ]
+};
+const T = {
+  color: "purple",
+  pieceShape: [
+    [0,1,0,0],
+    [1,1,1,0]
+  ]
+};
+const Z = {
+  color: "red",
+pieceShape: [
+    [0,1,1,0],
+    [0,0,1,1]
+ ]
+};
+function CreateSpawn(columns){
+  spawn = []
+  for (var i = 1; i <= columns; i++){
+    spawn.push(i)
+  }
+  spawncut = (columns-4)/2
+  spawn = spawn.slice(spawncut,-spawncut)
+  return spawn
+}
 const tiles = document.getElementsByClassName("tile");
